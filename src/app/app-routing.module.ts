56123989d4c242dab59c0b42322cc197fd74f2a5
Aykut Yararbas <aykutyararbas@yahoo.com>
2018-10-30 16:14:22 -0700

    First draft

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ToolbarComponent} from './toolbar/toolbar.component';
+import {CanvasComponent} from './canvas/canvas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tb1', component: CanvasComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { GroupComponent } from './toolbar/group/group.component';
+import { ItemComponent } from './toolbar/group/item/item.component';
+import {CanvasComponent} from './canvas/canvas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    CanvasComponent,
+    GroupComponent,
+    ItemComponent,
   ],
   imports: [
     BrowserModule,
